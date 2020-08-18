1cce5c36075d6cfa71c5d8b6aa4dd9a1cdcf6476
Subhrajyoti <subha.ionic3@gmail.com>
2020-08-18 12:55:54 +0530

    simple table view app

--- Project_1_Swift_iOS/ViewController.swift
+++ Project_1_Swift_iOS/ViewController.swift
@@ -8,11 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        performSelector(inBackground: #selector(self.loadData), with: nil)
+    }
+    
+    @objc func loadData() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        pictures = pictures.sorted()
+        performSelector(onMainThread: #selector(reloadTableViewData), with: nil, waitUntilDone: false)
+    }
+    
+    @objc func reloadTableViewData() {
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.titleStr = "Picture \(indexPath.row + 1) of \(pictures.count)"
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 
 
